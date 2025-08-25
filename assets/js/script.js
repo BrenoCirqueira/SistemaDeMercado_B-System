03766b5f4f4409e95f726c707744d71b72fcb20e
Breno Cirqueira <cirqueirabreno38@gmail.com>
2025-08-25 10:44:11 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+const produtos = [
+  { codigo: '001', nome: 'Arroz', preco: 20.00 },
+  { codigo: '002', nome: 'Feijão', preco: 8.50 },
+  { codigo: '003', nome: 'Macarrão', preco: 5.75 }
+];
+
+// inicializando a construção da logica
+const codigoDoProduto = document.getElementById("codigo")
+const valorDoProduto = document.getElementById("valor")
+const valorRecebido = document.getElementById("valorRecebido")
+const troco = document.getElementById("troco")
+const subtotal = document.getElementById("subtotal")
+const btnFinalizar = document.getElementById("botao")
+
+
+
